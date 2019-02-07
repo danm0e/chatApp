2e204fb97efe9c4b7c6cfd003b2a3026b381e72f
Dan Moe <dan.moe@designinc.co.uk>
2019-02-07 22:31:00 +0000

    add master list page content

---
+++ src/models/user.model.ts
@@ -0,0 +1,21 @@
+export interface User {
+	birth_year	: string;
+	created		: Date;
+	edited		: Date;
+	eye_color	: string;
+	films		: Array<string>;
+	gender		: string;
+	hair_color	: string;
+	height		: number;
+	homeworld	: string;
+	mass		: number
+	name		: string;
+	skin_color	: string;
+	species		: Array<string>;
+	starships	: Array<string>;
+	url			: string;
+	vehicles	: Array<string>;
+	thumbnail	: string;
+	featured_img: string;
+	bio 		: string;
+}
--- src/pages/master-list/master-list.ts
+++ src/pages/master-list/master-list.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage } from 'ionic-angular';
+import { IonicPage, LoadingController } from 'ionic-angular';
 import { StarWarsService } from './../../providers/star-wars/star-wars.service';
+import { User } from '../../models/user.model';
 
 @IonicPage()
 @Component({
@@ -9,10 +10,40 @@ import { StarWarsService } from './../../providers/star-wars/star-wars.service';
 })
 export class MasterListPage {
 
+	people: User[] = [];
+	likeCount = 12;
+	
 	constructor(
-		private api: StarWarsService ) {
+		private api: StarWarsService,
+		private loadCtrl: LoadingController ) {
 		// films / people / planets / species / starships / vehicles
-		this.api.logFeed('people');
+		// this.api.logFeed('people');
+		// show the loader whilst we're getting the data
+		const loader = this.loadCtrl.create({
+			content: 'Please wait...'
+		});
+		loader.present();
+		// make the api call
+		this.api.getFeed('people')
+			.then( people => {
+				// once resolved, hide the loader and push the data 
+				// to our array for looping through
+				loader.dismiss();
+				this.people = people['results'];
+
+				for(let user of this.people) {
+					user.thumbnail = '//placehold.it/35x35';
+					user.featured_img = '//placehold.it/280x170';
+					user.bio = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+				}
+						
+				console.log(this.people)
+			});
+			
+	}
+
+	like() {
+		this.likeCount++;
 	}
 
 }
