c1ccade38440078548782ff182232b86180fd4c8
Dan Moe <dan.moe@designinc.co.uk>
2019-02-07 20:03:33 +0000

    add sign out process to menu component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,50 +1,64 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
+import { Nav, Platform, LoadingController, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 
 @Component({
-  templateUrl: 'app.html'
+	templateUrl: 'app.html'
 })
 export class MyApp {
 
-  @ViewChild(Nav) nav: Nav;
+	@ViewChild(Nav) nav: Nav;
 
-  rootPage: string = 'SignInPage';
+	rootPage: string = 'SignInPage';
+	pages: Array<{ title: string, component: string }>;
 
-  pages: Array<{title: string, component: string}>;
+	constructor(
+		private platform: Platform,
+		private statusBar: StatusBar,
+		private splashScreen: SplashScreen,
+		private loadCtrl: LoadingController,
+		private menuCtrl: MenuController ) {
 
-  constructor(
-    public platform: Platform, 
-    public statusBar: StatusBar, 
-    public splashScreen: SplashScreen ) {
+		this.initializeApp();
 
-    this.initializeApp();
+		// used for an example of ngFor and navigation
+		this.pages = [
+			{ title: 'Dashboard', component: 'DashboardPage' },
+			{ title: 'Photos', component: 'PhotosPage' },
+			{ title: 'Available Missions', component: 'AvailableMissionsPage' },
+			{ title: 'My Missions', component: 'MyMissionsPage' },
+			{ title: 'Chat', component: 'ChatPage' }
+		];
 
-    // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Dashboard', component: 'DashboardPage' },
-      { title: 'Photos', component: 'PhotosPage' },
-      { title: 'Available Missions', component: 'AvailableMissionsPage' },
-      { title: 'My Missions', component: 'MyMissionsPage' },
-      { title: 'Chat', component: 'ChatPage' }
-    ];
+	}
 
-  }
+	initializeApp() {
+		this.platform.ready().then(() => {
+			// Okay, so the platform is ready and our plugins are available.
+			// Here you can do any higher level native things you might need.
+			this.statusBar.styleDefault();
+			this.splashScreen.hide();
+		});
+	}
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
+	/** sets current nav stack root */
+	openPage(page) {
+		this.nav.setRoot(page.component);
+	}
 
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
-  }
+	/** signs out, toggle menu draw and sets root to sign in view */
+	signOut() {
+		const loader = this.loadCtrl.create({
+			content: 'Signing out'
+		});
+		loader.present();
+		// mock sign out
+		setTimeout( () => {
+			this.menuCtrl.toggle();
+			this.openPage({ component: 'SignInPage' });
+			loader.dismiss();
+		}, 2000 );
+	}
 }
