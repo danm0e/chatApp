f26f99a907b5cc52985576bc4046acab88e8721c
Dan Moe <dan.moe@designinc.co.uk>
2019-02-09 21:04:31 +0000

    add refactored master list and single list sections. removes api calls from the child view.

--- src/pages/master-list-single/master-list-single.ts
+++ src/pages/master-list-single/master-list-single.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavParams } from 'ionic-angular';
 import { User } from '../../models/user.model';
-import { StarWarsService } from '../../providers/star-wars/star-wars.service';
 
 @IonicPage()
 @Component({
@@ -12,16 +11,15 @@ export class MasterListSinglePage {
 
 	user: User;
 	details = [];
-	exclude = ['thumbnail', 'featured_img', 'bio'];
+	exclude = ['thumbnail', 'featured_img', 'bio', 'url'];
 
-	constructor(
-		private navParams: NavParams,
-		private api: StarWarsService) {
-	}
+	constructor( 
+		private navParams: NavParams ){}
 
 	/** gets and sets data on view load */
 	ionViewWillLoad() {
 		this.user = this.navParams.data;
+		console.log(this.user);
 		this.getDetails();
 	}
 
@@ -30,31 +28,14 @@ export class MasterListSinglePage {
 		return Array.isArray(detail);
 	}
 
-	/** makes another api call for the current value */
-	async getNestedData(value: string) {
-		return await this.api.getFeed(value)
-	}
-
 	/** get the details for the view to loop through and display */
 	getDetails() {
-		// loop through the 
+		// loop through the profile data and build a key value pairing
+		// to render in the template
 		Object.keys(this.user)
 			.forEach( async key => {
 				let formattedKey = key.replace(/_/g, ' '),
-					value = this.user[key],
-					tempArr = [];
-
-				// if this item has nested values, 
-				// get the actual data from the api
-				if ( this.isArray(value) ) {
-					for (let subvalue of value) {
-						let nestedData = await this.getNestedData(subvalue),
-							firstItem = Object.keys(nestedData)[0];
-							tempArr.push(nestedData[firstItem]);
-					}
-					value = tempArr;
-				}
-
+					value = this.user[key];
 				// filter the data so we're not repeating anything and 
 				// we don't have any blank spaces
 				if( this.exclude.indexOf(key) < 0 && value.length > 0 ) {
--- src/pages/master-list/master-list.ts
+++ src/pages/master-list/master-list.ts
@@ -11,30 +11,76 @@ import { User } from '../../models/user.model';
 export class MasterListPage {
 
 	people: User[] = [];
-	
+	fetchedData: any;
+	profile = [];
+
 	constructor(
 		private api: StarWarsService,
-		private loadCtrl: LoadingController ) {
+		private loadCtrl: LoadingController) {
 		// films / people / planets / species / starships / vehicles
 		// this.api.logFeed('people');
-		// show the loader whilst we're getting the data
-		const loader = this.loadCtrl.create({
-			content: 'Please wait...'
-		});
-		loader.present();
-		// make the api call
-		this.api.getFeed(null, 'people')
-			.then( people => {
-				// once resolved, hide the loader and push the data 
-				// to our array for looping through
-				loader.dismiss();
-				this.people = people['results'];
+	}
 
-				for(let user of this.people) {
-					user.thumbnail = '//placehold.it/35x35';
-					user.featured_img = '//placehold.it/400x250';
-					user.bio = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
-				}
+	/** gets and sets data when the view will load */
+	async ionViewWillEnter() {
+		// if this is the first time the view is loaded
+		if (!this.fetchedData) {
+			// show the loader whilst we're getting the data
+			const loader = this.loadCtrl.create({
+				content: 'Please wait...'
 			});
+			loader.present();
+			// make the api call
+			this.fetchedData = await this.api.getFeed(null, 'people');
+			this.people = this.fetchedData['results'];
+
+			// sets faux placeholder data
+			for (let user of this.people) {
+				user.thumbnail = '//placehold.it/35x35';
+				user.featured_img = '//placehold.it/400x250';
+				user.bio = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+			}
+
+			this.profile = await this.buildProfile();
+			loader.dismiss();
+		}
+	}
+
+	/** returns true if details is an array */
+	isArray(detail: any) {
+		return Array.isArray(detail);
+	}
+
+	/** 
+	 * gets the full profile data for each character including 
+	 * values for nested data arrays. We can then pass the profile
+	 * object to the single view and avoid calling the api again
+	 */
+	async buildProfile() {
+		let results = this.fetchedData['results'],
+			profile = [];
+		// loop through the top level data array
+		for (let i = 0; i < results.length; i++) {
+			// loop through the object keys
+			Object.keys(results[i])
+				.forEach( async key => {
+					let value = results[i][key],
+						tempArr = [];
+					// if this item has nested values 
+					if (this.isArray(value)) {
+						// get the data from the api
+						for (let subvalue of value) {
+							let nestedData = await this.api.getFeed(subvalue),
+								firstItem = Object.keys(nestedData)[0];
+							// push data to holding array
+							tempArr.push(nestedData[firstItem]);
+						}
+						results[i][key] = tempArr;
+					}
+				});
+			// push the data to our profile array
+			profile.push(results[i]);
+		}
+		return profile;
 	}
 }
