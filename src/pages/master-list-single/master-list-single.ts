b6861ea66859d72831efc5eec0e176b57baf57ec
Dan Moe <dan.moe@designinc.co.uk>
2019-02-10 12:29:19 +0000

    add datetime formatting function to single list results

--- src/pages/master-list-single/master-list-single.ts
+++ src/pages/master-list-single/master-list-single.ts
@@ -33,9 +33,15 @@ export class MasterListSinglePage {
 		// loop through the profile data and build a key value pairing
 		// to render in the template
 		Object.keys(this.user)
-			.forEach( async key => {
+			.forEach( key => {
 				let formattedKey = key.replace(/_/g, ' '),
 					value = this.user[key];
+
+					// catch and format any dates
+					if( value.indexOf('000Z') > 0 ) {
+						value = this.formatDateTime(value)
+					}
+
 				// filter the data so we're not repeating anything and 
 				// we don't have any blank spaces
 				if( this.exclude.indexOf(key) < 0 && value.length > 0 ) {
@@ -43,5 +49,15 @@ export class MasterListSinglePage {
 				}
 			}
 		);
+	}
+
+	/**
+	* returns a formatted date and time string
+	* @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
+	*/
+	formatDateTime(dt: string) {
+        let date = new Date(dt),
+            options = { weekday: 'long', month: 'short', day: '2-digit', hour: 'numeric', minute: 'numeric' }
+        return date.toLocaleDateString("en-GB", options);
 	}
 }
